956b2b404b4e2d7c9fcd093bc67143cf9cdbd9cf
FinalprojekNurul <nh0835988@gmail.com>
2023-12-22 08:54:29 +0700

    FinalprojekNurul

---
+++ app/Models/MataKuliah.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswas extends Model
+{
+    use HasFactory;
+    protected $table = 'matakuliahs';
+    protected $fillable = [
+        'kode_mk',
+        'nama_mk',
+        'sks',
+        'semester',
+		];
+
+
+
+}
